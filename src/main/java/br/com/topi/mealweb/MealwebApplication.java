689d73461b30a0285943a058471e8e6ce7cdcde1
Gabriel Simão <gabrielsimao@hotmail.com>
2021-03-25 21:52:02 -0300

    Initial commit

---
+++ src/main/java/br/com/topi/mealweb/MealwebApplication.java
@@ -0,0 +1,13 @@
+package br.com.topi.mealweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/topi/mealweb/MealwebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.topi.mealweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
